e0fcab8ad9ef2e97f8efcedeaaf1349e7e75300f
Diadara <nithin111@gmail.com>
2013-07-03 19:10:32 +0530

    normalize added

--- silpa_common/__init__.py
+++ silpa_common/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
-from silpa_common.charmap import charmap
+from silpa_common.charmap import charmap, language
 from silpa_common.langdetect import detect_lang
+from silpa_common.normalizer import normalize, normalize_ml
