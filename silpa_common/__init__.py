5e4e9a870ea592383ef9ba79fbabe7a167ae3d72
Diadara <nithin111@gmail.com>
2013-08-09 17:51:33 +0530

    Revert "normalize added" as Normalizer module serves the purpose
    This reverts commit e0fcab8ad9ef2e97f8efcedeaaf1349e7e75300f.

--- silpa_common/__init__.py
+++ silpa_common/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
-from silpa_common.charmap import charmap, language
+from silpa_common.charmap import charmap
 from silpa_common.langdetect import detect_lang
-from silpa_common.normalizer import normalize, normalize_ml
