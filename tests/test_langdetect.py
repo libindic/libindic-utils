25ef70651342aca2270c493474c47314fcce1348
Vasudev Kamath <kamathvasudev@gmail.com>
2014-05-01 18:32:12 +0530

    Make tests compatible with both py3 and py2
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- tests/test_langdetect.py
+++ tests/test_langdetect.py
@@ -8,16 +8,16 @@ from silpa_common import servicemethod
 
 def test_langdetect():
     '''TEST: Language Detection'''
-    assert detect_lang(u'ನಮಸ್ಕಾರ').values() == ['kn_IN']
-    assert detect_lang(u'बॆंगलूरु').values() == ['hi_IN']
-    assert detect_lang(u'বাংগ্লা').values() == ['bn_IN']
-    assert detect_lang(u'മലയാളം').values() == ['ml_IN']
-    assert detect_lang(u'தமிள்').values() == ['ta_IN']
-    assert detect_lang(u'తెలుగు').values() == ['te_IN']
-    assert detect_lang(u'଒ରିଯା ').values() == ['or_IN']
-    assert detect_lang(u'ਪਂਜਾਬਿ').values() == ['pa_IN']
-    assert detect_lang(u'ગુજરાતિ').values() == ['gu_IN']
-    assert detect_lang("English").values() == ['en_US']
+    assert detect_lang(u'ನಮಸ್ಕಾರ')[u'ನಮಸ್ಕಾರ'] == 'kn_IN'
+    assert detect_lang(u'बॆंगलूरु')[u'बॆंगलूरु'] == 'hi_IN'
+    assert detect_lang(u'বাংগ্লা')[u'বাংগ্লা'] == 'bn_IN'
+    assert detect_lang(u'മലയാളം')[u'മലയാളം'] == 'ml_IN'
+    assert detect_lang(u'தமிள்')[u'தமிள்'] == 'ta_IN'
+    assert detect_lang(u'తెలుగు')[u'తెలుగు'] == 'te_IN'
+    assert detect_lang(u'଒ରିଯା')[u'଒ରିଯା'] == 'or_IN'
+    assert detect_lang(u'ਪਂਜਾਬਿ')[u'ਪਂਜਾਬਿ'] == 'pa_IN'
+    assert detect_lang(u'ગુજરાતિ')[u'ગુજરાતિ'] == 'gu_IN'
+    assert detect_lang("English")["English"] == 'en_US'
 
 
 def test_servicemethod():
