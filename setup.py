afda3fb7160ccc879527c6ee9394a35d6c12680c
Vasudev Kamath <kamathvasudev@gmail.com>
2014-05-01 15:55:28 +0530

    Use pbr for setup file and use setup.cfg
    Signed-off-by: Vasudev Kamath <kamathvasudev@gmail.com>

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-name = 'silpa_common'
 
 setup(
-    name=name,
-    version="0.1.1",
-    url="",
-    license="LGPL 3.0",
-    description="Common Library for SILPA modules",
-    author='Santhosh Thottingal',
-    author_email="santhosh.thottingal@gmail.com",
-    long_description="""This module is bundle of common functions used by modules developed for Project SILPA.
-Currently it includes charmap and langdetect functions.""",
-    include_package_data=True,
-    packages=find_packages(),
-    setup_requires=['setuptools-git'],
-    install_requires=['setuptools'],
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True
 )
